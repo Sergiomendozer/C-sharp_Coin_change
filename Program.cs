399eff34adcea4c8a6cdb37029f9ff4c559fb1e1
sergiomendozer <sergiomendozansx@gmail.com>
2022-05-15 15:05:02 -0700

    float instead of int

--- Program.cs
+++ Program.cs
@@ -1,11 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 // Console.WriteLine("Hello, World!");
-static intchange_to_give(int change){
-    return
+static float change_to_give(float change){
+    Console.WriteLine(change);
+    return (change);
 }
 
 Console.WriteLine("Enter change: ");
-int change = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine(change);
+float change = Single.Parse(Console.ReadLine());
+// Console.WriteLine(change);
 
 change_to_give(change);
