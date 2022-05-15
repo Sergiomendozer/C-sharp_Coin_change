ea3fc7c4c4c70ef9cadea2c4dae568d9eaa5656a
sergiomendozer <sergiomendozansx@gmail.com>
2022-05-14 19:37:45 -0700

    user input set up

--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Console.WriteLine("Hello, World!");
+static intchange_to_give(int change){
+    return
+}
+
+Console.WriteLine("Enter change: ");
+int change = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(change);
+
+change_to_give(change);
